5a945a4fe43b60affb90ccc24a105a2979c0a784
ERIK SCOLARO s343553 <s343553@studenti.polito.it>
2025-07-02 23:36:27 +0200

    feat: enabled validation pipe global and class serializer interceptor sto support input validation and output cato with @espose and @exclude

--- studexam-api/src/main.ts
+++ studexam-api/src/main.ts
@@ -1,8 +1,41 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true // we enable transform to automatically convert the payload too the desired type
+    // remember to specify in the signature of methods the types!
+  })); // Automatically manages validation of classes with calss-validator 
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+//To force query and param types to int or string the docuemtnation suggests this pattenr
+// 
+// import { IsNumberString } from 'class-validator';
+// 
+// export class FindOneParams {
+//  @IsNumberString()
+//  id: string;
+//}
+////
+
+// Joking ! you can do the autommatic cast this way 
+//@Get(':id/prodotto/:pid')
+//getProduct(
+//  @Param('id') id: number,
+//  @Param('pid') pid: number,
+//) {
+//  console.log(typeof id === 'number');  // true
+//  console.log(typeof pid === 'number'); // true
+//  // ...
+//}
+//
+
+// It seems like the automatic cast of single entry does not fork for the body
+// you need a dto object here, we need to verify that
