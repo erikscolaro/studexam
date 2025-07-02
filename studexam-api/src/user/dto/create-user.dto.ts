ece207de0a3167e7df77578263989efc8e055002
ERIK SCOLARO s343553 <s343553@studenti.polito.it>
2025-07-02 21:46:12 +0200

    feat: validation in dto user

--- studexam-api/src/user/dto/create-user.dto.ts
+++ studexam-api/src/user/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsOptional, IsString, Length, Matches } from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsString()
+  @Length(3, 50)
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Length(8, 100)
+  // es. almeno 1 maiuscola, 1 numero, 1 simbolo
+  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/, { message: 'Password too weak' })
+  password: string;
+
+  @IsString()
+  @Length(2, 2)
+  @IsOptional()
+  language?: string;
+}
